b0dc8e11ae2f9818a384fe7eab7d7493b66ba28b
hongzzi <hongzzi.dev@gmail.com>
2019-08-13 08:43:43 +0900

    0812 HOMEWORK

---
+++ 0812/Main_1158.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_1158{
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder("<");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int total = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> josephus = new LinkedList<Integer>();
+
+        //큐 채우기
+        for(int i =1; i<=total; i++)
+            josephus.add(i);
+
+        
+        int count = 0;
+
+        while (!josephus.isEmpty()){
+            int tmp = josephus.poll();
+            count++;
+
+            if (count == num) {
+                sb.append(tmp + ", ");
+                count = 0;
+            } else {
+                josephus.add(tmp);
+
+            }
+        }
+        sb.delete(sb.length()-2, sb.length());
+        sb.append(">");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
